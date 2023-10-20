194cf4f1406cb324aba9c39101dd5f8ded1d5e4b
User <User@DESKTOP-COLBM6H>
2023-10-20 22:25:44 +0300

    Add project files.

---
+++ WordAnalyzer_HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WordAnalyzer_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
